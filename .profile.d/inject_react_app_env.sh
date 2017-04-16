f71b8a3c2d4a56800d693f3f68f9e62092daa0d2
Weyert de Boer <weyert@rippleapp.it>
2017-04-16 19:20:07 +0100

    Attempt to fix the issue when script bundle could not be found

--- .profile.d/inject_react_app_env.sh
+++ .profile.d/inject_react_app_env.sh
@@ -14,11 +14,22 @@ then
 
   # Get exact filename.
   js_bundle_filename=`ls $js_bundle`
+  echo "Suggested bundle filename: $js_bundle_filename"
   
-  echo "Injecting runtime env into $js_bundle_filename (from .profile.d/inject_react_app_env.sh)"
+  if [ -f $js_bundle ]
+  then
+	  if [ -f /app/.heroku/create-react-app/injectable_env.rb ]
+	  then	
+		  echo "Injecting runtime env into $js_bundle_filename (from .profile.d/inject_react_app_env.sh)"
 
-  # Render runtime env vars into bundle.
-  ruby -E utf-8:utf-8 \
-    -r /app/.heroku/create-react-app/injectable_env.rb \
-    -e "InjectableEnv.replace('$js_bundle_filename')"
+		  # Render runtime env vars into bundle.
+		  ruby -E utf-8:utf-8 \
+		    -r /app/.heroku/create-react-app/injectable_env.rb \
+		    -e "InjectableEnv.replace('$js_bundle_filename')"
+	  else
+	  	  echo "Failed to find create-react-app/injectable_env.rb."
+	  fi  
+  else
+  	echo "Failed to find the bundle file"
+  fi
 fi
